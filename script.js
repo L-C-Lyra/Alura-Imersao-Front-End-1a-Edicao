466007de053ecbfae142454f03821b53479c2c5a
Lucas C Lyra Pereira <148347259+L-C-Lyra@users.noreply.github.com>
2024-01-25 11:24:00 -0300

    Modifications after 'CSS Grid, Media Queries and DOM Manipulation with JS - Class #04' are Closed.

---
+++ script.js
@@ -0,0 +1,37 @@
+// DOM = Document Object Model (Modelo de Objeto de Documentos).
+
+const searchInput = document.getElementById("search-input");
+const resultsArtist = document.getElementById("result-artist");
+const resultsPlaylist = document.getElementById("result-playlists");
+
+function requestApi(searchTerm) {
+  fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+    .then((response) => response.json())
+    .then((results) => displayResults(results));
+}
+
+function displayResults(results) {
+  hidePlaylists();
+  const artistName = document.getElementById("artist-name");
+  const artistImage = document.getElementById("artist-img");
+  results.forEach((element) => {
+    artistName.innerText = element.name;
+    artistImage.src = element.urlImg;
+  });
+  resultsArtist.classList.remove("hidden");
+}
+
+function hidePlaylists() {
+  resultsPlaylist.classList.add("hidden");
+}
+
+document.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultsPlaylist.classList.add("hidden");
+    resultsArtist.classList.remove("hidden");
+    return;
+  }
+
+  requestApi(searchTerm);
+});
